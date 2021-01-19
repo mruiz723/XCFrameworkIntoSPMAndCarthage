b60710e26774a0f51df537bda5039d7f09cf2b5f
Marlon David Ruiz Arroyave <Marlon.X.Ruiz.-ND@disney.com>
2021-01-19 12:55:46 -0500

    Reorganize the project to MVVM and connect views to LoginVC.

---
+++ XCFrameworkIntoSPMAndCarthage/XCFrameworkIntoSPMAndCarthage/Application/Login/View/ViewControllers/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  XCFrameworkIntoSPMAndCarthage
+//
+//  Created by Marlon David Ruiz Arroyave on 19/01/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - IBActions
+    @IBAction func logIn(_ sender: Any) {
+        
+    }
+    
+}
+
