c4a50b5dc58a641681379f07c5bd80bfb120426e
Marlon David Ruiz Arroyave <Marlon.X.Ruiz.-ND@disney.com>
2021-01-19 12:11:58 -0500

    Create the project.

---
+++ XCFrameworkIntoSPMAndCarthage/XCFrameworkIntoSPMAndCarthage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCFrameworkIntoSPMAndCarthage
+//
+//  Created by Marlon David Ruiz Arroyave on 19/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
